43d6c8159526b6a831c199ee82c6f527b20b173f
Stefan Luecke <git@aberrational.org>
2022-05-01 23:30:07 +0200

    Put "extern crate objc" behind conditional compile attribute

--- rui-io/src/lib.rs
+++ rui-io/src/lib.rs
@@ -1,15 +1,16 @@
 #![feature(thread_spawn_unchecked)]
 
 #[macro_use]
+#[cfg(any(target_os = "ios", target_os = "macos"))]
 extern crate objc;
 
 #[macro_use]
 extern crate lazy_static;
 
 pub mod event;
-pub mod surface;
-mod platform;
 mod os_error;
+mod platform;
+pub mod surface;
 
 pub use os_error::OsError;
 
